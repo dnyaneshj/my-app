4800c1234e459f044153b1536c74487868d2a8e9
dnyaneshj <dbjadhav077@gmail.com>
2022-10-30 16:16:43 +0530

    code cleaning

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Contact from './component/contact';
 import Localdata from "./component/localData";
-import { logInfo,logWarning,logError,logDebug } from './util/log-utils'
+import { logInfo, logWarning, logError, logDebug } from './util/log-utils'
 const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 function App() {
   const [post, setPost] = useState(null);
-  const [data, setData] = useState(null);
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       logInfo('succesfully fetch data');
@@ -16,53 +15,42 @@ function App() {
   }, []);
 
   useEffect(() => {
-   
+
     axios.get(baseURL).then((response) => {
       logInfo('api call is success');
       setData(response.data);
     }).catch(function (error) {
-    if (error.response.status === 404) {
-      //console.log(error.response.status);
-      logDebug(error.response);
-      logError('Request failed with status code 404', error.response.status);
-    } else {
-      logWarning(' The client never received a results, and the request was never left');
-    } 
-})
-}, []);
+      if (error.response.status === 404) {
+        //console.log(error.response.status);
+        logDebug(error.response);
+        logError('Request failed with status code 404', error.response.status);
+      } else {
+        logWarning(' The client never received a results, and the request was never left');
+      }
+    })
+  }, []);
 
-  if (!post){
-      return
-  } 
+  if (!post) {
+    return
+  }
 
   return (
     <>
-    <Localdata/>
-    <Contact/>
-   
-      {/* <table>
-        <thead><tr><th>title</th><th>body</th></tr></thead>
-        <tbody>
-          {
-            post.map((items, index) => (
-             <tr key={index}><td>{items.title}</td><td>{items.body}</td></tr>
-            )
-            )}
-        </tbody>
-      </table> */}
-
-      <br/>
-      {/* <table>
-        <thead><tr><th>title</th><th>body</th></tr></thead>
-        <tbody>
-          {
-            data?.map((items, index) => (
-             <tr key={index}><td>{items.title}</td><td>{items.body}</td></tr>
-            )
-            )}
-        </tbody>
-      </table> */}
-      {/* <Contact /> */}
+      <Localdata />
+      <div >
+        <table>
+          <thead><tr><th>title</th><th>body</th></tr></thead>
+          <tbody>
+            {
+              post.map((items, index) => (
+                <tr key={index}><td>{items.title}</td><td>{items.body}</td></tr>
+              )
+              )}
+          </tbody>
+        </table>
+      </div>
+      <Contact />
+      <br />
     </>
   );
 }
